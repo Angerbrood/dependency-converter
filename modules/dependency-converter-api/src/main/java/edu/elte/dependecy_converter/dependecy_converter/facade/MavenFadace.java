0796b20344f5993070a0b508182e5d8b73090da8
David Petz <petzdavid@gmail.com>
2018-06-30 10:57:58 +0200

    Removed unused facade

--- modules/dependency-converter-api/src/main/java/edu/elte/dependecy_converter/dependecy_converter/facade/MavenFadace.java
+++
@@ -1,7 +0,0 @@
-package edu.elte.dependecy_converter.dependecy_converter.facade;
-
-import edu.elte.dependecy_converter.dependecy_converter.reader.maven.MavenFileReader;
-
-public class MavenFadace {
-
-}
